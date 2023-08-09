16d33925c9ca746f79f55d59d8267c8e0dfa6735
MD Kamran khan <mohammadkkhan78@gmail.com>
2023-08-09 16:25:24 +0530

    ss

--- crudapp/settings.py
+++ crudapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&#@^al*_j048lw#kh^r-kr8#40^ddogkf^e2ah4w)ff-g=ec_6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app',".now_sh"]
 
 
 # Application definition
@@ -125,3 +125,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
